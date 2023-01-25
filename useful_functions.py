79fe4d7e0f5f1161cdc6d02fd4474c90d84d992b
MPica <morgane.pica@ens-lyon.fr>
2023-01-25 12:20:57 +0100

    initiating the central function py file

--- useful_functions.py
+++ useful_functions.py
@@ -1,6 +1,27 @@
 
 
+from SPARQLWrapper import SPARQLWrapper, SPARQLWrapper2, JSON, TURTLE, XML, RDFXML
+import numpy as np
+import pandas as pd
+
+
+
 def query_to_table(spql_queried):
+    
+    """
+    Function taking an endpoint associated with a SPARQL query
+    with the return format set on JSON, and returning the results
+    as a Pandas DataFrame.
+    The function will directly transmit the query to the endpoint,
+    receive and transform the results.
+    
+    :param spql_queried: A SPARQLWrapper.Wrapper.SPARQLWrapper object
+        to which are already defined :
+        - an endpoint with SPARQLWrapper()
+        - a result format with variable.setReturnFormat(JSON)
+        - a query to transmit with variable.setQuery()
+    
+    """
     
     preparing = {}
 
